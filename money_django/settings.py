baf264d73602158b1ce7c524e006684a833a2b68
Ubuntu <ubuntu@ip-172-31-27-81.us-west-2.compute.internal>
2015-07-22 11:14:28 +0000

    addded gitingore and removed no-csrf from settings

--- money_django/settings.py
+++ money_django/settings.py
@@ -48,8 +48,7 @@ REST_FRAMEWORK = {
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    #    'django.middleware.csrf.CsrfViewMiddleware',
-    'money_django.disable_csrf.DisableCSRF',
+    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
@@ -88,3 +87,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '/var/www/static/'
