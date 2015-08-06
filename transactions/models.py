ff59345639a24b2159a0b2949dd571e94fbcb60a
Ashutosh Dwivedi <adwivedi@akamai.com>
2015-08-07 00:46:47 +0530

    added creation_time,fixed models accordint to new rest_framework api

--- transactions/models.py
+++ transactions/models.py
@@ -1,18 +1,27 @@
 from django.db import models
 
+INR = 'INR'
+DOLLAR = 'DOLLAR'
+
+currencies = (
+    (INR, 'Indian Rupee'),
+    (DOLLAR, 'DOLLAR'),
+)
+
 class Transaction(models.Model):
     desc = models.CharField(max_length=255)
-    currency = models.CharField(max_length=255)
+    currency = models.CharField(max_length=255, choices=currencies)
     amount = models.FloatField()
+    creation_time = models.DateTimeField(auto_now=True)
     # user id of the person who paid
     # paidby = models.CharField(max_length=255)
 
 
 class Split(models.Model):
-    transaction = models.ForeignKey(Transaction, \
-                                    related_name='splits' )
+    transaction = models.ForeignKey(Transaction,
+                                    related_name='splits')
     userid = models.CharField(max_length=255)
-    split = models.IntegerField()
+    amount = models.IntegerField(blank=False)
 
     class Meta:
-        unique_together = ('transaction', 'userid')
+        pass #unique_together = ('transaction', 'userid')
--- transactions/serializers.py
+++ transactions/serializers.py
@@ -1,17 +1,36 @@
 from transactions.models import Transaction, Split
+from django.db import transaction
 from rest_framework import serializers
 from rest_framework.serializers import HyperlinkedRelatedField
 from rest_framework.serializers import \
      HyperlinkedModelSerializer
 
-class SplitSerializer(HyperlinkedModelSerializer):
+class SplitSerializer(serializers.ModelSerializer):
     class Meta:
         model = Split
-        fields = ('userid', 'split')
+        fields = ('userid', 'amount')
             
 class TransactionSerializer(serializers.ModelSerializer):
     splits = SplitSerializer(many=True)
     
     class Meta:
         model = Transaction
-        fields = ('desc', 'currency', 'amount', 'splits')
+        fields = ('desc', 'currency', 'amount', 'splits', 'creation_time')
+
+    @transaction.atomic
+    def create(self, validated_data):
+        return self.save_nested(validated_data)
+
+    @transaction.atomic
+    def update(self, instance, validated_data):
+        return self.save_nested(validated_data)
+
+    def save_nested(self, validated_data):
+        splits = validated_data.pop('splits')
+
+        transaction = Transaction.objects.create(**validated_data)
+        for split in splits:
+            spilts_obj, created = Split.objects.update_or_create(
+                transaction=transaction, **split)
+        return transaction
+
